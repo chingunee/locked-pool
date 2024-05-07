6cd3728dbc9c62f58b5f037ee4634962862a24f6
Chingun Amarbaatar <chingunee.dev@gmail.com>
2024-05-07 23:09:12 +0800

    modified security checks

--- contracts/LockedPool.sol
+++ contracts/LockedPool.sol
@@ -2,19 +2,20 @@
 pragma solidity ^0.8.24;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 import "./interface/ILockedPool.sol";
 import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /// @title Locked Pool
 /// @notice A staking pool for employments and market.
-contract LockedPool is ILockedPool, Pausable, Ownable2Step{
+contract LockedPool is ILockedPool, Pausable, Ownable2Step, ReentrancyGuard {
+    using SafeERC20 for IERC20;
     uint256 public unlockTimePoolOfMarket;
     uint256 public unlockTimePoolOfEmployments;
-
-    // (tokenAddress => isAllowedForStaking)
-    mapping(address => bool) public tokenAllowlist;
+    IERC20 public immutable token;
 
     // (tokenAddress => stakerAddress => stakedAmount)
     mapping(address => mapping(address => uint256)) public balanceOfMarket;
@@ -22,80 +23,67 @@ contract LockedPool is ILockedPool, Pausable, Ownable2Step{
     // (tokenAddress => stakerAddress => stakedAmount)
     mapping(address => mapping(address => uint256)) public balanceOfEmployments;
 
-    constructor(address _owner, uint256 _unlockTimePoolOfMarket, uint256 _unlockTimePoolOfEmployments) Ownable(_owner) {
+    constructor(address _owner, address _tokenAddress, uint256 _unlockTimePoolOfMarket, uint256 _unlockTimePoolOfEmployments) Ownable(_owner) {
+        require(_tokenAddress != address(0), "Token address cannot be zero");
         require(
             block.timestamp < _unlockTimePoolOfMarket && block.timestamp < _unlockTimePoolOfEmployments,
             "Unlock time should be in the future"
         );
+        token = IERC20(_tokenAddress);
         unlockTimePoolOfMarket = _unlockTimePoolOfMarket;
         unlockTimePoolOfEmployments = _unlockTimePoolOfEmployments;
-        // uint256 length = _tokensAllowed.length;
-        // for(uint256 i; i < length; ++i){
-        //     if (_tokensAllowed[i] == address(0)) revert TokenCannotBeZeroAddress();
-        //     tokenAllowlist[_tokensAllowed[i]] = true;
-        // }
-    }
-
-    function addTokenAllowlist(address[] memory _tokensAllowed) onlyOwner public{
-        uint256 length = _tokensAllowed.length;
-                for(uint256 i; i < length; ++i){
-            if (_tokensAllowed[i] == address(0)) revert TokenCannotBeZeroAddress();
-            tokenAllowlist[_tokensAllowed[i]] = true;
-        }
     }
 
     /*//////////////////////////////////////////////////////////////
                             Staker Functions
     //////////////////////////////////////////////////////////////*/
 
-    function depositForMarketPool(address _token, address _for, uint256 _amount) whenNotPaused external {
+    function depositForMarketPool(address _for, uint256 _amount) whenNotPaused external {
         if (_amount == 0) revert DepositAmountCannotBeZero();
         if (_for== address(0)) revert CannotDepositForZeroAddress();
-        if (!tokenAllowlist[_token]) revert TokenNotAllowedForStaking();
 
-        balanceOfMarket[_token][_for] += _amount;
+        address tokenAddress = address(token);
+        balanceOfMarket[tokenAddress][_for] += _amount;
+        emit Deposit(_for, tokenAddress, _amount);
 
-        emit Deposit(_for, _token, _amount);
-
-        IERC20(_token).transferFrom(msg.sender, address(this), _amount);   
+        token.safeTransferFrom(msg.sender, address(this), _amount);
     }
 
-    function depositForEmploymentPool(address _token, address _for, uint256 _amount) whenNotPaused external {
+    function depositForEmploymentPool(address _for, uint256 _amount) whenNotPaused external {
         if (_amount == 0) revert DepositAmountCannotBeZero();
         if (_for== address(0)) revert CannotDepositForZeroAddress();
-        if (!tokenAllowlist[_token]) revert TokenNotAllowedForStaking();
+        
+        address tokenAddress = address(token);
+        balanceOfEmployments[tokenAddress][_for] += _amount;
+        emit Deposit(_for, tokenAddress, _amount);
 
-        balanceOfEmployments[_token][_for] += _amount;
-
-        emit Deposit(_for, _token, _amount);
-
-        IERC20(_token).transferFrom(msg.sender, address(this), _amount);   
+        token.safeTransferFrom(msg.sender, address(this), _amount);
     }
 
-    function withdrawMarketPool(address _token, uint256 _amount) public {
-        if (_amount == 0) revert WithdrawAmountCannotBeZero();
+    function withdrawMarketPool(uint256 _amount) public nonReentrant whenNotPaused {
+        require(_amount > 0, "WithdrawAmountCannotBeZero");
         require(block.timestamp >= unlockTimePoolOfMarket, "You can't withdraw yet");
         
-        // Check if the user has sufficient balance to withdraw
-        require(balanceOfMarket[_token][msg.sender] >= _amount, "Insufficient balance");
+        address tokenAddress = address(token);
+        require(balanceOfMarket[tokenAddress][msg.sender] >= _amount, "Insufficient balance");
         
-        balanceOfMarket[_token][msg.sender] -= _amount;
-        emit Withdraw(msg.sender, _token, _amount);
+        balanceOfMarket[tokenAddress][msg.sender] -= _amount;
+        emit Withdraw(msg.sender, tokenAddress, _amount);
 
-        IERC20(_token).transfer(msg.sender, _amount);       
+        token.safeTransfer(msg.sender, _amount);
     }
 
-    function withdrawEmploymentPool(address _token, uint256 _amount) public {
-        if (_amount == 0) revert WithdrawAmountCannotBeZero();
+    function withdrawEmploymentPool(uint256 _amount) public nonReentrant whenNotPaused {
+        require(_amount > 0, "WithdrawAmountCannotBeZero");
         require(block.timestamp >= unlockTimePoolOfEmployments, "You can't withdraw yet");
-        
-        // Check if the user has sufficient balance to withdraw
-        require(balanceOfEmployments[_token][msg.sender] >= _amount, "Insufficient balance");
-        
-        balanceOfEmployments[_token][msg.sender] -= _amount;
-        emit Withdraw(msg.sender, _token, _amount);
 
-        IERC20(_token).transfer(msg.sender, _amount);
+        address tokenAddress = address(token);
+        require(balanceOfEmployments[tokenAddress][msg.sender] >= _amount, "Insufficient balance");
+
+        balanceOfEmployments[tokenAddress][msg.sender] -= _amount;
+        emit Withdraw(msg.sender, tokenAddress, _amount);
+
+        token.safeTransfer(msg.sender, _amount);
     }
 
     /**
@@ -107,7 +95,7 @@ contract LockedPool is ILockedPool, Pausable, Ownable2Step{
 
     /**
      * @inheritdoc ILockedPool
-     */
+    */
     function unpause() external onlyOwner whenPaused{
         _unpause();
     }
